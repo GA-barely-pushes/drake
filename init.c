93e7a0fd4bff484b4acff77b2c776cb91d95bb5a
GA-barely-pushes <zianmaraya127@gmail.com>
2026-04-29 18:16:19 +0800

    minor setup adjustment

--- init.c
+++ init.c
@@ -3,19 +3,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define ERR_X_SIZE -1
-#define ERR_Y_SIZE -2
-#define ERR_ALLOC_FAILED -3
+#define ERR_X_SIZE_TOO_SMALL -1
+#define ERR_X_SIZE_TOO_LARGE -2
+#define ERR_Y_SIZE_TOO_SMALL -3
+#define ERR_Y_SIZE_TOO_LARGE -4
+#define ERR_ALLOC_FAILED -5
+
+#define RESET "\e[0m"
+#define RED "\e[31m"
 
 char setup_game() {
   char
   result_of = setup_sizes();
 
-  if (result_of == ERR_X_SIZE)
-    return !!fputs("X axis too large or too small\n", stderr);
+  if (result_of == ERR_X_SIZE_TOO_SMALL)
+    return !!fputs(RED"\n  X axis too small\n"RESET, stderr);
+  if (result_of == ERR_X_SIZE_TOO_LARGE)
+    return !!fputs(RED"\n  X axis too large\n"RESET, stderr);
 
-  if (result_of == ERR_Y_SIZE)
-    return !!fputs("Y axis too large or too small\n", stderr);
+  if (result_of == ERR_Y_SIZE_TOO_SMALL)
+    return !!fputs(RED"\n  Y axis too small\n"RESET, stderr);
+  if (result_of == ERR_Y_SIZE_TOO_LARGE)
+    return !!fputs(RED"\n  Y axis too large\n"RESET, stderr);
 
   result_of = populate_grid();
 
@@ -40,14 +49,18 @@ char setup_sizes() {
   fputs("  What is the width of your game?\n    ~> ", stdout);
   scanf("%hi", &x);
 
-  if (x > 255 || x <= SAFE_ZONE * 2)
-    return ERR_X_SIZE;
+  if (x <= SAFE_ZONE * 2)
+    return ERR_X_SIZE_TOO_SMALL;
+  if (x > 255)
+    return ERR_X_SIZE_TOO_LARGE;
 
   fputs("  What is the height of your game?\n    ~> ", stdout);
   scanf("%hi", &y);
 
-  if (y > 255 || y <= SAFE_ZONE * 2)
-    return ERR_Y_SIZE;
+  if (y <= SAFE_ZONE * 2)
+    return ERR_Y_SIZE_TOO_SMALL;
+  if (y > 255)
+    return ERR_Y_SIZE_TOO_LARGE;
 
   game.size.x = x;
   game.size.y = y;
